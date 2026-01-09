23052c2acbd4d3a3ed4fe6165892da3d1e47fa83
hasemansi <hasemansi233@gmail.com>
2026-01-08 17:06:58 -0800

    model setup completed

--- backend/qrmenu_backend/restaurant/admin.py
+++ backend/qrmenu_backend/restaurant/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Restaurant, Category, MenuItem
+
+admin.site.register(Restaurant)
+admin.site.register(Category)
+admin.site.register(MenuItem)
